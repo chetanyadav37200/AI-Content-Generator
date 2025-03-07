59a9c794b63c3734e31640899ffe6f6e0305e85c
Chetan Yadav <chetanyadav372002@gmail.com>
2025-03-07 18:28:01 +0530

    Fixed UserButton appearance issue

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Search } from 'lucide-react';
 import { UserButton } from '@clerk/nextjs';
 
 function Header() {
   return (
     <div className='p-5 shadow-sm border-b-2 flex justify-between items-center'>
       <div className='ml-auto'> {/* Push UserButton to the right */}
-        <UserButton afterSignOutUrl='/' appearance={{ layout: { avatarSize: 48 } }} /> {/* Make UserButton bigger */}
+      <UserButton afterSignOutUrl="/" appearance={{ elements: { avatarBox: { width: 48, height: 48 } } }} />
+
       </div>
     </div>
   );
